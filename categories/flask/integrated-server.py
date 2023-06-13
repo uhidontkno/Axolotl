500c7f6424aac2f5850455dfec1f826e9e447483
uhidontkno <86028086+uhidontkno@users.noreply.github.com>
2023-06-13 14:34:55 -0400

    add integrated server to measure uptime

---
+++ categories/flask/integrated-server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import threading
+app = Flask(__name__)
+
+@app.route('/')
+def ping():
+    return 'Axolotl is online!', 200
+
+def run_flask_server():
+    app.run(host='0.0.0.0', port=3000)
+print("Starting integrated server.")
+# Start the Flask server in a separate thread
+flask_thread = threading.Thread(target=run_flask_server)
+flask_thread.start()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import os
 from discord.ext import commands
-
-client = commands.Bot(command_prefix="a!", intents=discord.Intents.all())
+import threading
+import signal
+import sys
+import asyncio
+client = commands.Bot(command_prefix="", intents=discord.Intents.all())
 token = ""
 
 with open("token.env") as f:
@@ -14,7 +17,18 @@ for filename in os.listdir("./categories"):
         client.load_extension(f"categories.{filename[:-3]}")
         print(f"Loaded extension categories.{filename[:-3]}...")
 
-print(f"Loaded all extensions! Logging into discord...")
+print(f"Loaded all extensions! Logging into Discord...")
+
+
+def run_integrated_server():
+    print("SERVER: Starting integrated server...")
+    os.system("py categories/flask/integrated-server.py")
+
+
+# Start the integrated server on a separate thread
+server_thread = threading.Thread(target=run_integrated_server)
+server_thread.start()
+
 
 # Add event handlers
 @client.event
@@ -22,10 +36,20 @@ async def on_ready():
     # Log a message to indicate that the bot is online
     print("*water swishes*, I'm online!")
 
+
 @client.event
 async def on_slash_command_error(ctx, ex):
     # Log the error message to the console
     print(f"An error occurred while processing slash command in {ctx.guild.id}: {ex}")
+
+def stop_bot(signal, frame):
+    print("STOP DROP AND ROLL!")
+    os.system("taskkill /f /im py.exe")
+
+# Register the signal handler for CTRL+C
+signal.signal(signal.SIGINT, stop_bot)
+
+
 
 # Start the bot
 client.run(token)
